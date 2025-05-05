49333527ee4d7e8d6ca916e1461a1f813892566a
TIKIDAXX <HAP.MESTRE77@GMAIL.COM>
2025-05-05 22:11:42 +0200

    Create insert.sql

---
+++ script_servers/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO temas (id, titulo, ruta_md, categoria) VALUES
+(5, 'I) Introduccion', 'db/01_intro.md', 'Database Management'),
+(11, 'II) Relationships and Entities', 'db/02_relationships.md', 'Database Management'),
+(12, 'III) Normalization', 'db/03_normalizacion.md', 'Database Management'),
+(13, 'IV) SQL', 'db/04_SQL.md', 'Database Management'),
+(14, 'V) MySql', 'db/05_mysql.md', 'Database Management'),
+(16, 'VI) PL/SQL', 'db/06_PLSQL.md', 'Database Management');
